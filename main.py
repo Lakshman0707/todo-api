223da260e32672b3e30963e56bd73a638801ea2a
lakshman j <lakshmanj0707@gmail.com>
2026-06-20 10:35:37 +0530

    Added health endpoint

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ from models import Todo
 from schemas import TodoCreate, TodoResponse
 import models
 
+# Create tables
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Todo API",
+    version="1.0.0"
+)
 
+# Health Check Route
 @app.get("/")
 def home():
     return {"message": "Todo API is running"}
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+# Database Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -21,36 +31,52 @@ def get_db():
     finally:
         db.close()
 
+# CREATE TODO
 @app.post("/todos", response_model=TodoResponse)
 def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
     db_todo = Todo(
         title=todo.title,
         description=todo.description
     )
+
     db.add(db_todo)
     db.commit()
     db.refresh(db_todo)
+
     return db_todo
 
+# READ ALL TODOS
 @app.get("/todos", response_model=list[TodoResponse])
 def get_todos(db: Session = Depends(get_db)):
     return db.query(Todo).all()
 
+# READ SINGLE TODO
 @app.get("/todos/{todo_id}", response_model=TodoResponse)
 def get_todo(todo_id: int, db: Session = Depends(get_db)):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
 
     if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Todo not found"
+        )
 
     return todo
 
+# UPDATE TODO
 @app.put("/todos/{todo_id}", response_model=TodoResponse)
-def update_todo(todo_id: int, updated_todo: TodoCreate, db: Session = Depends(get_db)):
+def update_todo(
+    todo_id: int,
+    updated_todo: TodoCreate,
+    db: Session = Depends(get_db)
+):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
 
     if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Todo not found"
+        )
 
     todo.title = updated_todo.title
     todo.description = updated_todo.description
@@ -60,12 +86,16 @@ def update_todo(todo_id: int, updated_todo: TodoCreate, db: Session = Depends(ge
 
     return todo
 
+# DELETE TODO
 @app.delete("/todos/{todo_id}")
 def delete_todo(todo_id: int, db: Session = Depends(get_db)):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
 
     if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Todo not found"
+        )
 
     db.delete(todo)
     db.commit()
